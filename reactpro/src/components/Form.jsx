0800f42573c0959a4c738f48a5c908e0b7ec5ea0
Gab <gabriel_al4k@yahoo.com>
2025-06-17 17:29:24 +0800

    multiple input

--- reactpro/src/components/Form.jsx
+++ reactpro/src/components/Form.jsx
@@ -3,27 +3,36 @@ import { useState } from "react";
 
 export default function Form(){
 
-    const [name, setName] = useState("");
+    const [name, setName] = useState({firstName: "", lastName: ""});
 
-    const [email, setEmail] = useState("");
+    // const [email, setEmail] = useState("");
     // function handleChange(event) {
     //     // console.log(event.target.value);
     //     setName(event.target.value);
     // }
 
-    function handleChange2(event) {
-        // console.log(event.target.value);
-        setEmail(event.target.value);
-    }
+    // function handleChange2(event) {
+    //     // console.log(event.target.value);
+    //     setEmail(event.target.value);
+    // }
 
     return (
         <div>
             <form >
-                <input onChange={ (e) => setName(e.target.value)} type="text" value={name} />
+                <input 
+                onChange={ (e) => setName({...name, firstName: e.target.value})} 
+                type="text" 
+                value={name.firstName} />
                 &nbsp;
                 <br />
                 <br />
-                <input onChange={ handleChange2} type="text" value={email} />
+                <input 
+                onChange={ (e) => setName({...name, lastName: e.target.value})} 
+                type="text" 
+                value={name.lastName} />
+                
+
+                {/* <input onChange={ handleChange2} type="text" value={email} /> */}
             </form>
         </div>
     );
